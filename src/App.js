b9f0c8c05608882e46b416f06363feb7ba5faed9
U-GHTINC\derni <derni@DERNI-TMX514.ghtinc.com>
2024-02-21 14:55:32 +0800

    feature 1.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye!
+          Release 1.0
         </p>
         <a
           className="App-link"
